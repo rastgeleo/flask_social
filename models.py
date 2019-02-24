3b513214d40e42b37480a5ac295fa96625140837
S Jeon <rastgeleo@gmail.com>
2019-02-24 12:10:11 +0000

    Add models.py

---
+++ models.py
@@ -0,0 +1,31 @@
+import datetime
+
+from peewee import *
+from flask.ext.login import UserMixin
+from flask.ext.bcrypt import generate_password_hash, check_password_hash
+
+DATABASE = sqlitedatabase('social.db')
+
+class User(UserMixin, Model):
+    username = CharField(unique=True)
+    email = CharField(unique=True)
+    password = CharField(max_length=100)
+    joined_at = DateTimeField(default=datetime.datetime.now)
+    is_admin = BooleanField(default=False)
+    
+    class Meta:
+        database = DATABASE
+        order_by = ('-joined_at',)
+        
+    @classmethod
+    def create_user(cls, username, email, password, admin=False):
+        try:
+            cls.create(
+                username=username,
+                email=email,
+                password=generate_password_hash(password),
+                is_admin=admin
+            )
+            except IntegrityError:
+                raise ValueError("The value is incorrect")
+            
