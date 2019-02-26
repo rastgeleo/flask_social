6b4a2cd8c82e8d14cdb547c1a62232820ab6a71c
S Jeon <rastgeleo@gmail.com>
2019-02-26 21:33:03 +0000

    Add forms.py

---
+++ forms.py
@@ -0,0 +1,45 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import (DataRequired, Reqexp, ValidationError, Email, 
+                                Length, EqualTo) 
+
+from models import User
+
+def name_exists(form, field):
+    if User.select().where(User.username == field.data).exists():
+        raise ValidationError('User with that name already exists.')
+        
+def email_exists(form, field):
+    if User.select().where(User.email == field.data).exists():
+        raise ValidationError('User with that email already exists.')
+        
+class RegisterForm(Form):
+    username = StringField(
+        'username',
+        validators=[
+            DataRequired(),
+            Regexp(
+                r'^[a-zA-Z0-9_]+$',
+                messages=("Username should be one word, letters, "
+                          "numbers and underscore only")
+            ),
+            name_exists
+        ])
+    email = StringField(
+        'Email',
+        validators=[
+            DataRequired(),
+            Email(),
+            email_exists
+        ])
+    password = PasswordField(
+        'Password',
+        validators=[
+            DataRequired(),
+            Length(min=2),
+            EqualTo('password2', message='Passwords must match')
+        ])
+    password2 = PasswordField(
+        'Confirm Password',
+        validators=[DataRequired()]
+    )
